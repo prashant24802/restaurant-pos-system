73b1be39c493e657d8fc72bc442a8ef448a54d74
prashant24802 <info.prashant248@gmail.com>
2026-07-11 00:48:16 +0530

    Implement Angular Material menu management page

--- restaurant-pos-frontend/src/app/features/menu/menu-list/menu-list.ts
+++ restaurant-pos-frontend/src/app/features/menu/menu-list/menu-list.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { Menu } from '../services/menu';
 import { MenuItem } from '../models/menu-item';
@@ -7,7 +14,15 @@ import { MenuItem } from '../models/menu-item';
 @Component({
   selector: 'app-menu-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
   templateUrl: './menu-list.html',
   styleUrl: './menu-list.css'
 })
@@ -17,13 +32,21 @@ export class MenuList implements OnInit {
 
   menuItems: MenuItem[] = [];
 
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'category',
+    'price',
+    'actions'
+  ];
+
+  search = '';
+
   ngOnInit(): void {
-
     this.loadMenu();
-
   }
 
-  loadMenu() {
+  loadMenu(): void {
 
     this.menuService.getAll().subscribe({
 
