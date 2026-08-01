0eaf9cb1c0539a153ab7f767d47024e95ee6aa45
prashant24802 <info.prashant248@gmail.com>
2026-08-02 00:40:05 +0530

    Fix environment import path

--- restaurant-pos-frontend/src/app/core/services/auth.ts
+++ restaurant-pos-frontend/src/app/core/services/auth.ts
@@ -1,8 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
-
+import { environment } from '../../../environments/environment';
 export interface LoginRequest {
   email: string;
   password: string;
